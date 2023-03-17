6e041498c9947d1566f1a2bec02d10c5325a208a
rachid <me@example.com>
2023-03-16 17:59:27 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the function does the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
